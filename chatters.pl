d9dddacc45c179cd6fcff2c293887d0a5e177ef5
Arvydas Sidorenko <asido4@gmail.com>
2012-05-01 21:21:21 +0200

    Added history

--- chatters.pl
+++ chatters.pl
@@ -15,6 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+#
+# History:
+#
+#	2012-05-01, Arvydas Sidorenko <asido4@gmail.com>
+# 		Version 0.1: initial release
+#
 
 use strict;
 use warnings;
